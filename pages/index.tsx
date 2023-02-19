fb73c5cd7a70a6d7aafc2111f9a63aee3058a6aa
Antraxmin <antraxmin@naver.com>
2023-02-19 18:45:55 +0900

    [#1] add header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,12 @@
+import Category from "@/components/common/Category";
+import Content from "@/components/common/Content";
+import Header from "@/components/common/Header";
+import { Fragment } from "react";
+
 export default function Home() {
-  return <div>Hello world!</div>;
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
 }
