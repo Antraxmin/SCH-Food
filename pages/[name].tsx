fbf682ec0c6e0b31b7db5231669ca414a38b321a
Antraxmin <antraxmin@naver.com>
2023-02-21 02:06:11 +0900

    [#8] get store data from json

--- pages/[name].tsx
+++ pages/[name].tsx
@@ -1,5 +1,5 @@
 import { Store } from "@/types/store";
-import { NextPage } from "next";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 
 interface Props {
   store: Store;
@@ -7,4 +7,23 @@ interface Props {
 
 const StoreDetail: NextPage<Props> = ({ store: Store }) => {
   return <div>식당 상세 정보 페이지입니다.</div>;
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const stores = (await import("../public/data.json")).default;
+  const paths = stores.map((store) => ({ params: { name: store.name } }));
+  return { paths, fallback: false };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const stores = (await import("../public/data.json")).default;
+  const store = stores.find((store) => store.name === params?.name);
+
+  // 요청한 식당 이름에 해당하는 데이터가 없다면 404 출력
+  if (!store) {
+    return {
+      notFound: true,
+    };
+  }
+  return { props: { store } };
 };
