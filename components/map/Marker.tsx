0ca783cfb60500e4623e6ea9099ae0223425a3a2
Antraxmin <antraxmin@naver.com>
2023-02-20 15:55:17 +0900

    [#5] 초기 렌더링 시 지도에 마커 객체 생성

--- components/map/Marker.tsx
+++ components/map/Marker.tsx
@@ -1,5 +1,17 @@
-const Marker = () => {
-  const { data: map } = useSWR<NaverMap>(MAP_KEY);
+import { useEffect } from "react";
+import { Marker } from "../../types/map";
+
+const Marker = ({ map, coordinates, onClick }: Marker) => {
+  useEffect(() => {
+    let marker: naver.maps.Marker | null = null;
+    // 지도 객체가 존재한다면 새로운 마커 객체 생성
+    if (map) {
+      marker = new naver.maps.Marker({
+        map: map, // 표시할 지도 객체
+        position: new naver.maps.LatLng(...coordinates), // 표시할 위치
+      });
+    }
+  });
 };
 
 export default Marker;
