2a7bbf6391ac545df87c01aaca734d53789e0655
Antraxmin <antraxmin@naver.com>
2023-02-20 15:48:00 +0900

    [#5] add marker type

--- types/map.ts
+++ types/map.ts
@@ -1,1 +1,8 @@
+import type { Coordinates } from "./store";
 export type NaverMap = naver.maps.Map;
+
+export type Marker = {
+  map: NaverMap;
+  coordinates: Coordinates;
+  onClick?: () => void;
+};
