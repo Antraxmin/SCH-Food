b8ccc787b70d6e0887e3445c5dc1d0de51d194d3
Antraxmin <antraxmin@naver.com>
2023-02-20 16:25:55 +0900

    [#5] 지도에 마커 띄우기 성공

--- components/map/MapSection.tsx
+++ components/map/MapSection.tsx
@@ -1,5 +1,7 @@
+import useMap from "@/hooks/useMap";
 import { NaverMap } from "@/types/map";
 import Map from "./Map";
+import Markers from "./Markers";
 
 const MapSection = () => {
   const { initializeMap } = useMap();
@@ -7,10 +9,12 @@ const MapSection = () => {
     initializeMap(map); // 지도가 load될때 실행 - 지도 객체를 전역상태로 저장
   };
 
-  return <Map onLoad={onLoadMap} />;
+  return (
+    <>
+      <Map onLoad={onLoadMap} />
+      <Markers />
+    </>
+  );
 };
 
 export default MapSection;
-function useMap(): { initializeMap: any } {
-  throw new Error("Function not implemented.");
-}
