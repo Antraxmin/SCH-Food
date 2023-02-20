4ebd2529c58c30ad882ef5b8f21d96410f8423b0
Antraxmin <antraxmin@naver.com>
2023-02-20 15:34:29 +0900

    [#5] update useMap hooks

--- hooks/useMap.ts
+++ hooks/useMap.ts
@@ -1,4 +1,20 @@
+import { NaverMap } from "@/types/map";
+import { useCallback } from "react";
+import { mutate } from "swr";
 import type { Coordinates } from "../types/store";
 
+export const MAP_KEY = "/map";
 export const INITIAL_CENTER: Coordinates = [36.773756, 126.933502];
 export const INITIAL_ZOOM = 17;
+
+const useMap = () => {
+  const initializeMap = useCallback((map: NaverMap) => {
+    mutate(MAP_KEY, map); // MAP_KEY에 지도 객체를 전역으로 저장
+  }, []);
+
+  return {
+    initializeMap,
+  };
+};
+
+export default useMap;
