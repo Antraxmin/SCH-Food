7f390a1b4758415a09c46780af194602f2d241e9
Antraxmin <antraxmin@naver.com>
2023-02-20 15:16:28 +0900

    [#4] update global state

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,22 @@ import CardSection from "@/components/card/CardSection";
 import Category from "@/components/common/Category";
 import Header from "@/components/common/Header";
 import MapSection from "@/components/map/MapSection";
-import { Fragment } from "react";
+import useStore from "@/hooks/useStore";
+import { Store } from "@/types/store";
+import { NextPage } from "next";
+import { Fragment, useEffect } from "react";
 
-export default function Home() {
+interface Props {
+  stores: Store[];
+}
+
+const Home: NextPage<Props> = ({ stores }) => {
+  const { initializeStores } = useStore();
+
+  useEffect(() => {
+    initializeStores(stores);
+  }, [initializeStores, stores]);
+
   return (
     <Fragment>
       <Header />
@@ -19,11 +32,13 @@ export default function Home() {
       </main>
     </Fragment>
   );
-}
+};
+
+export default Home;
 
 export async function getStaticProps() {
-  const stores = (await import('../public/data.json')).default;
+  const stores = (await import("../public/data.json")).default;
   return {
-    props: { stores}
-  }
+    props: { stores },
+  };
 }
