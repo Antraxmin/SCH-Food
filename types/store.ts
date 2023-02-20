6361fe9c038b091f1d167efb690bd76a9ae929f4
Antraxmin <antraxmin@naver.com>
2023-02-20 12:09:12 +0900

    [#4] add store api res types

--- types/store.ts
+++ types/store.ts
@@ -2,3 +2,16 @@ type latitude = number;
 type longitude = number;
 
 export type Coordinates = [latitude, longitude];
+
+export type Menu = { name: string; price: string };
+export type Store = {
+  id: number;
+  name: string;
+  description: string;
+  kind: string;
+  coordinates: Coordinates;
+  phone: string;
+  images: string[];
+  address: string;
+  menus: Menu[];
+};
