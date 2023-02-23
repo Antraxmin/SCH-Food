b5d305b37d8970991fe3cdd35090e6f54a8cf0e0
Antraxmin <antraxmin@naver.com>
2023-02-23 19:04:27 +0900

    [#11] 동적라우팅 구현 성공

--- pages/[name].tsx
+++ pages/[name].tsx
@@ -1,21 +1,51 @@
-import { Store } from "@/types/store";
+import DetailPage from "@/components/detail/DetailPage";
+import useCurrentStore from "@/hooks/useCurrentStore";
+import type { Store } from "@/types/store";
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import { useRouter } from "next/router";
 
 interface Props {
   store: Store;
 }
 
-const StoreDetail: NextPage<Props> = (store) => {
-  return <div>식당 상세 페이지 테스트</div>;
+const StoreDetail: NextPage<Props> = ({ store }) => {
+  const expanded = true;
+  const router = useRouter();
+  const { setCurrentStore } = useCurrentStore();
+  const goToDetail = () => {
+    setCurrentStore(store);
+    router.push(`/${store.name}`);
+  };
+  return (
+    <>
+      <DetailPage currentStore={store} expanded={expanded} />
+    </>
+  );
 };
 
 export default StoreDetail;
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const paths = [{ params: { name: "test" } }];
+  const stores = (await import(`${process.env.NEXT_PUBLIC_DB_URL}`)).default;
+  const paths = stores.map((store: { name: any }) => ({
+    params: { name: store.name },
+  }));
+
   return { paths, fallback: false };
 };
 
-export const getStaticProps: GetStaticProps = async () => {
-  return { props: {} };
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const stores = (await import(`${process.env.NEXT_PUBLIC_DB_URL}`)).default;
+  const store = stores.find(
+    (store: { name: string | string[] | undefined }) =>
+      store.name === params?.name
+  );
+
+  if (!store) {
+    return {
+      notFound: true, // 404 error
+    };
+  }
+
+  return { props: { store } };
 };
