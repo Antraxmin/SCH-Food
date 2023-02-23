29501e13393227ec987bfd4b2affbdf31984282a
Antraxmin <antraxmin@naver.com>
2023-02-23 15:24:51 +0900

    [#11] modify SWR currentstore type

--- components/List/ListSection.tsx
+++ components/List/ListSection.tsx
@@ -3,11 +3,14 @@ import { Store } from "@/types/store";
 import ListItem from "./ListItem";
 import useSWR from "swr";
 import useCurrentStore, { CURRENT_STORE_KEY } from "@/hooks/useCurrentStore";
+import DetailPage from "../detail/DetailPage";
+import { useState } from "react";
 
 const ListSection = () => {
   const { data: stores } = useSWR<Store[]>(STORE_KEY);
-  const { data: currentStore } = useSWR<Store[]>(CURRENT_STORE_KEY);
+  const { data: currentStore } = useSWR<Store>(CURRENT_STORE_KEY);
   const { setCurrentStore, clearCurrentStore } = useCurrentStore();
+  const [expanded, setExpanded] = useState(false);
 
   if (!stores) return null;
   return (
@@ -28,6 +31,7 @@ const ListSection = () => {
             onClick={() => {
               setCurrentStore(store); // 클릭한 식당을 currentStore로 지정
               console.log(store.name);
+              <DetailPage currentStore={currentStore} expanded={expanded} />;
             }}
           />
         );
