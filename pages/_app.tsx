93b73585c97a46ad0947481d6db95302522f40e2
Antraxmin <antraxmin@naver.com>
2023-02-28 00:36:06 +0900

    add google tagmanager script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-Z65Z9EQ3SK}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-Z65Z9EQ3SK');
+    `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
