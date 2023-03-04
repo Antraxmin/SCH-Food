aefebafaa04a3ca135c24b9562c30b9f43cde75a
Antraxmin <antraxmin@naver.com>
2023-03-04 12:00:03 +0900

    [#13] 학식 카테고리 횡스크롤 처리

--- components/univ/UnivCategory.tsx
+++ components/univ/UnivCategory.tsx
@@ -11,9 +11,9 @@ export default function UnivCategory() {
   const router = useRouter();
 
   return (
-    <div className="fixed z-30 flex justify-center w-full max-w-md mt-12 overflow-auto bg-white menubox top-4">
+    <div className="univmenubox w-full justify-center bg-white mt-12 fixed top-4 z-30 max-w-md overflow-auto">
       <ul className="bg-white menu menu-horizontal">
-        <li>
+        <li className="px-0">
           <Link href={"/univ/category/src"}>향설1관</Link>
         </li>
         <li>
