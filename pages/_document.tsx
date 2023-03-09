742b91d88c49c1c262cc3af81336e8601594cf4e
Chaemin-Lim <antraxmin@naver.com>
2023-03-09 16:42:02 +0900

    add google ads client code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,16 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="ko">
       <Head>
+        <Script
+          async
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}`}
+          strategy="lazyOnload"
+          crossOrigin="anonymous"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
