b2012b5cd805908704f226809fe53f652254f9b2
Chaemin-Lim <antraxmin@naver.com>
2023-06-29 02:16:05 +0900

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,12 +5,17 @@ export default function Document() {
   return (
     <Html lang="ko">
       <Head>
-        <Script
-          async
-          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}`}
-          strategy="lazyOnload"
-          crossOrigin="anonymous"
-        />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6656698385318639"
+     crossorigin="anonymous"></script>
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-format="fluid"
+     data-ad-layout-key="-gi+8-1l-2i+ar"
+     data-ad-client="ca-pub-6656698385318639"
+     data-ad-slot="7268392294"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
